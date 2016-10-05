bf175e0cbb78004eb76c3ecdf0e6eabbb9db37c5
Alexey Shevchenko <otetz@me.com>
2016-10-05 20:04:06 +0300

    NullHandler fallback for early Python versions.

--- iata_codes/__init__.py
+++ iata_codes/__init__.py
@@ -1,3 +1,9 @@
 import logging
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+try:
+    logging.getLogger(__name__).addHandler(logging.NullHandler())
+except AttributeError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+    logging.getLogger(__name__).addHandler(NullHandler())
