cc2d730e101fcb29898685426f90f9161872767e
Alexey Shevchenko <otetz@me.com>
2016-10-05 20:16:03 +0300

    All supported versions of Python

--- setup.py
+++ setup.py
@@ -21,7 +21,12 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
     ],
     packages=find_packages(),
